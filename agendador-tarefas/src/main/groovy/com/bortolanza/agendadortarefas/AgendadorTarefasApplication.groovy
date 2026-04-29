16ced0e8d49db30ed03fbf5068b7c203a76523d6
Junior Bortolanza <gbortolanzajr@gmail.com>
2026-04-29 09:44:11 -0300

    initial commit

---
+++ agendador-tarefas/src/main/groovy/com/bortolanza/agendadortarefas/AgendadorTarefasApplication.groovy
@@ -0,0 +1,13 @@
+package com.bortolanza.agendadortarefas
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AgendadorTarefasApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AgendadorTarefasApplication, args)
+	}
+
+}
