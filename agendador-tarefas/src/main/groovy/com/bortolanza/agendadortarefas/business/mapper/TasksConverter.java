f654dcaefa90bcff7c36f5841426abe40099a1ac
Junior Bortolanza <gbortolanzajr@gmail.com>
2026-04-29 16:16:02 -0300

    feat: added mapping

--- agendador-tarefas/src/main/groovy/com/bortolanza/agendadortarefas/business/mapper/TasksConverter.java
+++ agendador-tarefas/src/main/groovy/com/bortolanza/agendadortarefas/business/mapper/TasksConverter.java
@@ -3,11 +3,15 @@ package com.bortolanza.agendadortarefas.business.mapper;
 import com.bortolanza.agendadortarefas.business.dto.TasksDTO;
 import com.bortolanza.agendadortarefas.infrastructure.entity.TasksEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TasksConverter {
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "eventDate", target = "eventDate")
+    @Mapping(source = "creationDate", target = "creationDate")
 
     TasksEntity forTaskEntity(TasksDTO dto);
 
